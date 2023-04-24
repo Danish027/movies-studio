66dc741a16c7d31bb4929d1e5e32f6b2787f0257
Danish <danishqes0786@gmail.com>
2023-04-24 21:54:56 +0530

    Second Commit

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -5,14 +5,14 @@ import "./style.scss";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
-    return (
-        <div className="pageNotFound">
-            <ContentWrapper>
-                <span className="bigText">404</span>
-                <span className="smallText">Page not found!</span>
-            </ContentWrapper>
-        </div>
-    );
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!!!</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default PageNotFound;
