73298aef68e5bf351e06d85acf9ab5b6c7f3327e
Danish <danishqes0786@gmail.com>
2023-05-11 18:28:13 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { store } from "./store/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
